89bfcc3f9c01e90535796802f36d00605ca9cf63
Stephen Breuhaus <sbreuhaus@gmail.com>
2017-01-29 23:25:13 -0500

    start

---
+++ HTML5_Video_Player/script.js
@@ -0,0 +1,7 @@
+const player = document.querySelector('.player');
+const videp = player.querySelector('.viewer')
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
